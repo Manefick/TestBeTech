dd89d8594cbc57b519cc5ad32df10eb3b7565df4
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-11-04 16:12:49 +0200

    Добавьте файлы проекта.

---
+++ TestBeTech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestBeTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
